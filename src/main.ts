844a47747c7865ca1e9e0466b883e3e549c853aa
Wésley Guimarães <wesley.coder@gmail.com>
2024-01-02 02:18:22 -0300

    feat: first state machine

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,40 @@
+import './polyfills'
+import { assign, createActor, createMachine } from 'xstate'
+
+type GameContext = {
+  tick: number
+}
+
+const gameMachine = createMachine({
+  id: 'game',
+  initial: 'starting',
+  context: {
+    tick: 0,
+  },
+  types: {
+    context: {} as GameContext,
+  },
+  states: {
+    starting: {
+      on: {
+        init: 'idle',
+      },
+    },
+    idle: {
+      on: {
+        tick: {
+          target: '',
+          actions: assign(({ context: c }) => ({ tick: c.tick + 1 })),
+        },
+      },
+    },
+  },
+})
+
+const game = createActor(gameMachine)
+game.start()
+game.send({ type: 'init' })
+
 export const loop = () => {
+  game.send({ type: 'tick' })
 }
